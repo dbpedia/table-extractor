f4571989fe5658a2bcdf6a6427c7e4dbbc66b62b
Luca <lucav48@gmail.com>
2017-06-14 22:17:08 +0200

    [mapping_rules.py] review organization mapping rules

--- table_extractor/mapping_rules.py
+++ table_extractor/mapping_rules.py
@@ -1,18 +1,51 @@
-# coding=utf-8
-
+# coding = utf-8 
 # Mapping rules used to map table's data, topics are used to evaluate functions with same name.
 
-MAPPING_RULES_IT = {
-    'Regular season': 'resource/stagioneregolare',
-    'Playoffs': 'resource/playoff',
-    'Altre statistiche - Rimb.': 'resource/partiteRimbalzi',
-    'Altre statistiche - Rubate' : 'resource/matchStolen'
-    }
+MAPPING_RULES_IT={'League': 'dbo:league', 
+'GS': 'resource/GameSteal', 
+'Name': 'dbo:name', 
+'Weight': 'dbo:weight', 
+'GP': 'resource/gamePlayed', 
+'Hometown': 'dbo:hometown', 
+'Altre statistiche - Rubate': 'resource/matchStolen', 
+'Regular season': 'resource/stagioneregolare', 
+'Height': 'dbo:height', 
+'Altre statistiche - Rimb.': 'resource/partiteRimbalzi', 
+'RPG': 'RPgame', 
+'Year': 'dbo:year', 
+'WNBA regular season': 'resource/RegularSeason', 
+'Playoffs': 'resource/playoff', 
+'MPG': 'resource/MPG', 
+'Season': 'dbo:season', 
+'Team': 'dbo:team'}
 
-MAPPING_RULES_EN = {
-    'Regular season': 'resource/stagioneregolare',
-    'Playoffs': 'resource/playoff',
-    'Altre statistiche - Rimb.': 'resource/partiteRimbalzi',
-    'Altre statistiche - Rubate' : 'resource/matchStolen'
-    }
+
+MAPPING_RULES_FR={'S': 'ee', 
+'MPM': 'aa', 
+'FGA': 'd'}
+
+
+MAPPING_RULES_ES={'Temporada regular': 'sss', 
+'Predecesor: Elvin Hayes': '',
+'Enlaces externos': 'ssssss'}
+
+
+MAPPING_RULES_EN={'League': 'dbo:league', 
+'GS': 'resource/GameSteal', 
+'Name': 'dbo:name', 
+'GP': 'resource/gamePlayed', 
+'Year': 'dbo:year', 
+'Regular season': 'resource/stagioneregolare', 
+'Team': 'dbo:team', 
+'WNBA regular season': 'resource/RegularSeason', 
+'MPG': 'resource/MPG', 
+'Playoffs': 'resource/playoff', 
+'Weight': 'dbo:weight', 
+'Hometown': 'dbo:hometown', 
+'Altre statistiche - Rubate': 'resource/matchStolen', 
+'Height': 'dbo:height', 
+'RPG': 'RPgame', 
+'Altre statistiche - Rimb.': 'resource/partiteRimbalzi', 
+'Season': 'dbo:season'}
+
 
