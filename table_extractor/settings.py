4becaf45fb0f79e1d3952d2aa4aeab9ac47a1341
Luca <lucav48@gmail.com>
2017-06-16 23:57:57 +0200

    [settings.py] unique file settings

--- table_extractor/settings.py
+++ table_extractor/settings.py
@@ -2,73 +2,6 @@
 
 # This file contains some default configuration for the table_extractor. Please, be aware of customization!
 
-# Here you have a general description of what the script (pyTableExtraction.py) do
-GENERAL_DESCRIPTION = "This script try to parse data from tables in wiki pages.\n " \
-                      " To do so, it uses a WYSIWYG approach, using mapping rules " \
-                      "over cells of data depending on topic, wiki chapter and on some other parameters." \
-                      "Data found are reorganized in a RDF dataset and serialized in turtle format."
-
-# String of help for -s option
-SINGLE_HELP = "Use -s | --single to specify a single Wikipedia resource to parse. \n \
-                                    Eg: -s Andrea_Pirlo"
-# DEFAULT topic
-TOPIC_DEFAULT = 'elections'
-
-# Help for -t option
-TOPIC_HELP = "Use -t | --topic to specify a set of wiki pages to work with. "
-
-
-""" Topic you can choose from as -t parameter choice. Eg: -t actors
-     CUSTOMIZATION NOTE: Please ensure to add both the topic here and the relative sparql where_clause in
-     TOPIC_SPARQL_WHERE_CLAUSE."""
-TOPIC_CHOICES = ['all', 'soccer', 'actors', 'directors', 'writers', 'elections', 'elections_USA']
-
-
-""" Sparql where clause divided by topic, to access them use TOPIC_SPARQL_WHERE_CLAUSE[topic]
-   CUSTOMIZATION NOTE: please ensure that the where clause use ?s as the main variable"""
-TOPIC_SPARQL_WHERE_CLAUSE = {
-                'soccer': "?s a <http://dbpedia.org/ontology/SoccerPlayer>",
-
-                'elections': "?s a <http://dbpedia.org/ontology/Election>",
-
-                'elections_USA': "?s <http://it.dbpedia.org/property/wikiPageUsesTemplate> \
-                             <http://it.dbpedia.org/resource/Template:Elezioni_negli_Stati_Uniti_d'America>",
-
-                'actors': "?s a <http://dbpedia.org/ontology/Actor>",
-
-                'directors': "?film <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Film>. \
-                             ?film <http://dbpedia.org/ontology/director> ?s",
-
-                'writers': "?s a <http://dbpedia.org/ontology/Writer>"
-
-                }
-
-# Help for -w option
-WHERE_HELP = "Use -w | --where to specify a custom SPARQL where clause, \
-                                    capable of select a subset of wiki pages to work with. \n \
-                                    Note: you have to use ?s as the final query output. \n \
-                                    Eg: \"?s a <http://dbpedia.org/ontology/Writer>\" to select all \
-                                    pages describing writers. "
-
-# MODE you can choose from, as -m parameter. Eg: -m json
-MODE_CHOICES = ['html', 'h', 'json', 'j']
-# MODE is a dictionary fulfilled with aliases for html and json
-MODE = {'html': 'html',
-        'h': 'html',
-        'json': 'json',
-        'j': 'json'
-        }
-
-# DEFAULT chapter
-CHAPTER_DEFAULT = 'it'
-# Help for -c option
-CHAPTER_HELP = "Language of Wikipedia pages/resources to analyze. \n \
-                Please ensure you are using an existing wikipedia chapter tag! \n \
-                Eg: 'it' ---> it.wikipedia.org \n \
-                'en' ---> en.wikipedia.org \n \
-                'fr' ---> fr.wikipedia.org  etc. etc. \n \
-                DEFAULT = "+CHAPTER_DEFAULT
-
 # When using a online service, number of seconds between two tries
 SECONDS_BTW_TRIES = 2
 # Max number of attempts to contact a web service.
@@ -86,12 +19,10 @@ SPARQL_CALL_FORMAT = "&format=application%2Fsparql-results%2Bjson&debug=on"
 JSONPEDIA_BASE_URL = "http://jsonpedia.org/annotate/resource/json/"
 
 # Queries to select a list of resources and the number of resources involved
-SPARQL_RES_LIST_QUERY = ["SELECT distinct ?s as ?res WHERE{", "} LIMIT 1000 OFFSET "]
+SPARQL_RES_LIST_QUERY = ["SELECT distinct ?s as ?res WHERE{", "} LIMIT 5 OFFSET "]
 
 SPARQL_NUM_RES_QUERY = ["select (count(distinct ?s) as ?res_num) where{", "}"]
 
-# where clause to ensure that ?s has a wiki page
-WIKI_PAGE = ". ?s <http://dbpedia.org/ontology/wikiPageID> ?f"
 
 # path for dictionary created by pyDomainExplorer
 PATH_DOMAIN_EXPLORER = "../domain_settings.py"
@@ -101,3 +32,68 @@ COMMENT_MAPPING_RULES = "# coding = utf-8 \n# Mapping rules used to map table's 
 
 # mapping rule's prefix
 MAPPING_RULE_PREFIX = "MAPPING_RULES_"
+
+# identify row section
+ROW_SUFFIX = "row"
+
+# SETTINGS FOR pyDomainExplorer
+CODING_DOMAIN = "# coding = utf-8"
+RESEARCH_TYPE ="RESEACH_TYPE"
+DOMAIN_TITLE = "DOMAIN_EXPLORED"
+MAIN_PROPERTY = "MAIN_PROPERTY"
+CHAPTER = "CHAPTER"
+SECTION_NAME = "SECTION_"
+CHARACTER_SEPARATOR ="_tte_"
+SECTION_NAME_PROPERTY = "sectionProperty"
+ROW_TABLE_PROPERTY ="rowTableProperty"
+COMMENT_FOR_EXAMPLE_PAGE ="# Example page where it was found this section: "
+RESOURCE_FILE ="RESOURCE_FILE"
+# Query for getting DBpedia class resources
+SPARQL_GET_RESOURCES = ["select ?s where{ ", "}"]
+
+# Query for verifying if there is a resource with a particular property
+SPARQL_CHECK_PROPERTY = ["select ?s where{"," ?s rdfs:label ", ". } LIMIT 5"]
+
+PREFIX_MAPPING_RULE = "MAPPING_RULES_"
+
+# Path where the pyTableExtractor dictionary is located
+PATH_ACTUAL_DICTIONARY = "../table_extractor/mapping_rules.py"
+
+# Path where pyDomainExplorer print the result file .py
+FILE_PATH_DOMAIN_EXPLORED = "../domain_settings.py"
+
+# Help for verbose input
+VERBOSE_DEFAULT = '1'
+VERBOSE_CHOISES = [1, 2, 3]
+VERBOSE_HELP =" Verbose can be 1,2,3"
+
+
+# Help for chapter input
+CHAPTER_DEFAULT = 'en'
+
+CHAPTER_HELP = "Language of Wikipedia pages/resources to analyze. \n \
+                Please ensure you are using an existing wikipedia chapter tag! \n \
+                Eg: 'it' ---> it.wikipedia.org \n \
+                'en' ---> en.wikipedia.org \n \
+                'fr' ---> fr.wikipedia.org  etc. etc. \n \
+                DEFAULT = "+CHAPTER_DEFAULT
+
+# Here you have a general description of what the script (pyTableExtraction.py) do
+GENERAL_DESCRIPTION = "This script try to parse data from tables in wiki pages.\n " \
+                      " To do so, it uses a WYSIWYG approach, using mapping rules " \
+                      "over cells of data depending on topic, wiki chapter and on some other parameters." \
+                      "Data found are reorganized in a RDF dataset and serialized in turtle format."
+
+# Help for topic input
+TOPIC_HELP = "Topic input"
+
+# Single HELP
+SINGLE_HELP = "Search for a single resource, named like wikipedia page"
+
+WHERE_HELP = "Define a correct where clause"
+
+# define different topic selected by user
+TOPIC_WHERE = "SPARQL where clause defined"
+
+
+PATH_FOLDER_RESOURCE_LIST = "../Resource_lists"
