1b90779953b3ef68f101aa8b335317ac17c198ed
Luca <lucav48@gmail.com>
2017-06-16 23:55:32 +0200

    [pyDomainExplorer.py] review code and extended output file

--- domain_explorer/pyDomainExplorer.py
+++ domain_explorer/pyDomainExplorer.py
@@ -1,15 +1,15 @@
 # coding = utf-8
 
-import settings_domain_explorer as settings
 import sys
 import rdflib
 import ExplorerTools
+from table_extractor import settings
 from collections import OrderedDict
 
 # All table's section found
 all_sections = OrderedDict()
 all_headers = OrderedDict()
-
+example_wikipedia_pages = []
 __author__ = "Luca Virgili"
 
 """
@@ -47,13 +47,25 @@ def write_sections_and_headers(chapter, topic, verbose):
     domain_explored_file.write(settings.CODING_DOMAIN + "\n")
     domain_explored_file.write(settings.DOMAIN_TITLE + "='" + topic + "' \n")
     domain_explored_file.write(settings.CHAPTER + "='" + chapter + "' \n")
-    domain_explored_file.write(settings.RESEARCH_TYPE + "='" + explorer_tools.research_type + "' \n\n")
+    domain_explored_file.write(settings.RESEARCH_TYPE + "='" + explorer_tools.research_type + "' \n")
+    domain_explored_file.write(settings.RESOURCE_FILE + "='" + explorer_tools.get_res_list_file() + "' \n\n")
+    domain_explored_file.write(settings.MAIN_PROPERTY + "='" + "' \n\n")
     for key, dict in all_sections.items():
-        domain_explored_file.write(settings.SECTION_NAME + key.replace(" ", "_").replace("-", "_") + "={\n")
-        explorer_tools.print_dictionary_on_file(domain_explored_file, dict, verbose,all_headers)
+        wikipedia_example = explorer_tools.replace_accents(get_example_wikipedia_pages(key))
+        domain_explored_file.write(settings.COMMENT_FOR_EXAMPLE_PAGE + wikipedia_example + "\n")
+        key = explorer_tools.replace_accents(key.replace(" ", "_").replace("-", "_"))
+        domain_explored_file.write(settings.SECTION_NAME + key + "={\n")
+        explorer_tools.print_dictionary_on_file(domain_explored_file, dict, verbose, all_headers)
         domain_explored_file.write("} \n\n")
     domain_explored_file.close()
 
+
+def get_example_wikipedia_pages(section):
+    # each element in example_wikipedia_pages --> [0] page name where there is section name    [1]section name
+    for element in example_wikipedia_pages:
+        section_page = element.split(settings.CHARACTER_SEPARATOR)
+        if section_page[1] in section:
+            return section_page[0]
 
 """
 For each resource found in the domain, I get section and headers
@@ -81,15 +93,15 @@ def get_resource_sections_and_headers(chapter, topic, graph, res_name):
             html_parser.analyze_tables()
             for table in html_parser.all_tables:
                 if table.n_rows > 1:
-                    check_if_section_is_present(table.table_section, table.headers_refined)
+                    check_if_section_is_present(table.table_section, table.headers_refined, res_name)
 
 """
 Check if section is present in the dictionary of pyTableExtractor
 """
 
 
-def check_if_section_is_present(string_to_check, headers_refined):
-    section_name = check_if_similar_section_is_present(string_to_check)
+def check_if_section_is_present(string_to_check, headers_refined, res_name):
+    section_name = check_if_similar_section_is_present(string_to_check, res_name)
     # Check if this section was already created, if not it will create another dictionary
     check_if_headers_not_present_then_add(headers_refined, section_name)
 
@@ -114,23 +126,32 @@ def check_if_header_already_exists(header, section_name):
     try:
         all_sections[section_name][header]
     except KeyError:
-        header_property = check_if_propery_exists(header)
-        all_sections[section_name].__setitem__(header, header_property)
-        all_headers.__setitem__(header, header_property)
+        header_property = check_if_property_exists(header)
+        # delete headers that have only one character
+        if len(header) > 1:
+            all_sections[section_name].__setitem__(header, header_property)
+            all_headers.__setitem__(header, header_property)
 
 """
 Query dbpedia for searching a particular property.
 """
 
-def check_if_propery_exists(header):
+
+def check_if_property_exists(header):
     property = ""
     try:
         property = all_headers[header]
     except KeyError:
         answer = explorer_tools.make_sparql_dbpedia("check_property", header)
         if not isinstance(answer, basestring):
+            property_found = 0
             for row in answer["results"]["bindings"]:
-                property = "dbo:" + header.lower()
+                # sparql results can be wikidata or dbpedia uri, i have to filter to catch only dbpedia ontology uri
+                if "ontology" in row["s"]["value"] and property_found == 0:
+                    # i can't put a resource that can be a disambiguation or property --> I need ontology elements
+                    if not "disambiguation" in row["s"]["value"]:
+                        property = explorer_tools.get_ontology_name_from_uri(row["s"]["value"])
+                        property_found = 1
     return property
 
 
@@ -140,7 +161,7 @@ header to map)
 """
 
 
-def check_if_similar_section_is_present(string_to_check):
+def check_if_similar_section_is_present(string_to_check, res_name):
     keys = list(all_sections.keys())
     new_key = string_to_check
     similar_key = [key for key in keys if string_to_check.lower() in key.lower() or key.lower() in string_to_check.lower()]
@@ -156,6 +177,7 @@ def check_if_similar_section_is_present(string_to_check):
         else:
             all_sections[new_key] = OrderedDict()
             all_sections[new_key].__setitem__(settings.SECTION_NAME_PROPERTY, "")
+            example_wikipedia_pages.append(res_name + settings.CHARACTER_SEPARATOR + new_key)
     else:
         new_key = equal_key
     return new_key
@@ -181,11 +203,12 @@ def search_equal_key(array_string, string_to_check):
 When a I found a new key that's not present in the old dictionary, I will add it.
 """
 
+
 def insert_propertiers_old_dictionary(actual_dictionary):
     for actual_key in actual_dictionary:
         for section_key in all_sections:
             if actual_key == section_key:
-                all_sections[section_key].__setitem__(settings.SECTION_NAME_PROPERTY,actual_dictionary[actual_key])
+                all_sections[section_key].__setitem__(settings.SECTION_NAME_PROPERTY, actual_dictionary[actual_key])
             else:
                 for headers_key in all_sections[section_key]:
                     if actual_key == headers_key:
