a905ca42b158586a39b2cd28a5c524404c669342
Luca <lucav48@gmail.com>
2017-06-24 16:24:41 +0200

    [Selector.py] moved to domain_explorer package, solved bug in fetching resources

--- domain_explorer/Selector.py
+++ domain_explorer/Selector.py
@@ -8,16 +8,18 @@ __author__ = 'papalinis - Simone Papalini - papalini.simone.an@gmail.com'
 class Selector:
     """
     Class Selector is used to select which kind of wiki pages and so resources, have to be used in table's analysis.
-    A selector object creates a .txt file filled with resources' name. Then this file will be used by the analyzer to have a
-    set of resources to work with.
+    A selector object creates a .txt file filled with resources' name. Then this file will be used by the analyzer to
+    have a set of resources to work with.
 
     Arguments:
-        - utils (Utilities object) a Utilities object, useful to call dbpedia service depending on the chapter of interest
+        - utils (Utilities object) a Utilities object, useful to call dbpedia service depending on the chapter of
+    interest
 
     Public methods:
         - collect_resources() # Used to start the process of resource collection
 
     """
+
     def __init__(self, utils):
         """
         :param utils: a Utilities object which some methods are used to call a endpoint service.
@@ -30,14 +32,15 @@ class Selector:
         self.last_res_list = None
         self.current_res_list = []
 
-        # set the  SPARQL query used therefore to retrieve a list of 1000 resources at a time, see settings_domain_explorer.py.
+        # set the  SPARQL query used therefore to retrieve a list of 1000 resources at a time.
+        # see settings_domain_explorer.py.
         self.query_res_list = table_extractor.settings.SPARQL_RES_LIST_QUERY[0] + str(self.where_clause) + \
-                              table_extractor.settings.SPARQL_RES_LIST_QUERY[1]
+            table_extractor.settings.SPARQL_RES_LIST_QUERY[1]
 
         """ set the  SPARQL query used to know the total number of resources involved in collection.
            It then will be used by the collect_resources() method to augment the offset till the end of resources set"""
         self.query_num_res = table_extractor.settings.SPARQL_NUM_RES_QUERY[0] + str(self.where_clause) + \
-                             table_extractor.settings.SPARQL_NUM_RES_QUERY[1]
+            table_extractor.settings.SPARQL_NUM_RES_QUERY[1]
 
         # set total number of resources interested in this topic passing the query_num_res to utils.tot_res_interested()
         self.tot_res_interested = self.utils.tot_res_interested(self.query_num_res)
@@ -94,7 +97,7 @@ class Selector:
                         # encode res_name in utf-8
                         res_name = res_name.encode('utf-8')
                         # write the resource in the file with a newline tag
-                        self.list.write(str(res_name)+'\n')
+                        self.list.write(str(res_name) + '\n')
                         # update the number of resources serialized
                         self.resources_serialized += 1
 
@@ -122,6 +125,14 @@ class Selector:
         self.offset += 1000
 
     def set_where_clause(self):
+        """
+        Build where clause of sparql query according to user's parameters.
+        There are three possible cases:
+        - dbpedia ontology class (-t)
+        - single resource (-s)
+        - where clause given by user (-w)
+        :return:
+        """
         if self.utils.research_type == "t":
             return "?s ?o <http://dbpedia.org/ontology/" + self.utils.topic + ">"
         elif self.utils.research_type == "s":
