4cbdf62707921c7b49c9c3c6b903d2eec86f8bed
Luca <lucav48@gmail.com>
2017-08-10 20:22:41 +0200

    [settings.py] added comment variables, changed variables that explain script's parameters

--- table_extractor/settings.py
+++ table_extractor/settings.py
@@ -1,17 +1,18 @@
 # coding=utf-8
 
-
 # This file contains some default configuration for the table_extractor. Please, be aware of customization!
 
 # SETTINGS FOR pyTableExtractor
 
 # When using a online service, number of seconds between two tries
 SECONDS_BTW_TRIES = 2
+
 # Max number of attempts to contact a web service.
 MAX_ATTEMPTS = 3
 
 # Timeout for url request (write that in seconds)
 REQUEST_TIMEOUT = 5
+
 # Following are values used to compose urls of web services.
 
 SPARQL_CALL_FORMAT = "&format=application%2Fsparql-results%2Bjson&debug=on"
@@ -34,7 +35,7 @@ PATH_ACTUAL_DICTIONARY = "../table_extractor/mapping_rules.py"
 
 # SETTINGS FOR pyDomainExplorer
 # strings for settings file's header
-CODING_DOMAIN = "# coding = utf-8"
+CODING_DOMAIN = "# coding = utf-8 \n"
 RESEARCH_TYPE = "RESEACH_TYPE"
 VERBOSE_TYPE = "VERBOSE_VALUE"
 DOMAIN_TITLE = "DOMAIN_EXPLORED"
@@ -43,14 +44,20 @@ SECTION_NAME = "SECTION_"
 CHARACTER_SEPARATOR = "_tte_"
 SECTION_NAME_PROPERTY = "sectionProperty"
 FIRST_COMMENT = "# Comments below will help you in filling this file settings. Remember to change only " +\
-                SECTION_NAME + " variables.\n# Please do not modify research parameters. \n\n# Research parameters "
+                SECTION_NAME + " variables.\n# Please do not modify pyDomainExplorer parameters. \n\n" \
+                               "# pyDomainExplorer parameters "
 COMMENT_FOR_EXAMPLE_PAGE = "# Example page where it was found this section: "
 COMMENT_SECTION_PROPERTY = "# The entry named " + SECTION_NAME_PROPERTY + " represents ontology property associated " +\
     "to table's section.\n" \
     "# (Eg. in basket domain, section named playoff can be mapped with something like 'playoff' or "\
     " 'playoffMatch').\n# Triple example: <http://dbpedia.org/resource/Kobe_Bryant> " \
     "<http://dbpedia.org/ontology/playoffMatch>\n# <http://dbpedia.org/resource/Kobe_Bryant__1>"
+COMMENT_FILLED_ELEMENT = "# If you see elements already filled, it means that I have already found that header" \
+                         " in pyTableExtractor dictionary\n# or on dbpedia ontology\n" \
+                         "# If you empty a field that was filled, you will delete that rule from dictionary."
+
 RESOURCE_FILE = "RESOURCE_FILE"
+END_OF_FILE = "\n# END OF FILE \n"
 
 # Path where pyDomainExplorer print the result file .py
 FILE_PATH_DOMAIN_EXPLORED = "../domain_settings.py"
@@ -58,7 +65,9 @@ FILE_PATH_DOMAIN_EXPLORED = "../domain_settings.py"
 # Help for verbose input
 VERBOSE_DEFAULT = '1'
 VERBOSE_CHOISES = [1, 2]
-VERBOSE_HELP = " Verbose can be 1,2"
+VERBOSE_HELP = " Verbose can be 1,2. Verbose 1 list all headers for each section, while verbose 2 write only one" \
+               " time a header.( Eg. if two sections named 'playoff' and 'regular season' has header 'Year', in " \
+               " verbose 2 you will see 'Year' only one time in file settings) "
 
 # Help for chapter input
 CHAPTER_DEFAULT = 'en'
@@ -77,13 +86,17 @@ GENERAL_DESCRIPTION = "This script try to parse data from tables in wiki pages.\
                       "Data found are reorganized in a RDF dataset and serialized in turtle format."
 
 # Help for topic input
-TOPIC_HELP = "Topic input"
+TOPIC_HELP = "Topic input. In this field you have to one of the mapping classes defined on dbpedia. (Eg." \
+             "BasketballPlayer, Broadcaster) Go there: http://mappings.dbpedia.org/server/ontology/classes/ " \
+             "to see all available classes."
 
 # Single clause message help
-SINGLE_HELP = "Search for a single resource, named like wikipedia page"
+SINGLE_HELP = "Search for a single resource, named like wikipedia page. Remember to include" \
+              " underscore in resource's name. (Eg. Kobe_Bryant or Mick_Jagger)"
 
 # Where clause message help
-WHERE_HELP = "Define a correct where clause"
+WHERE_HELP = "Define a correct SPARQL where clause. It's important to name '?s' results given by query." \
+             "(Eg. -w ' ?s foaf:name ?name.  FILTER(?name = \"Kobe Bryant\"@it) "
 
 # define different topic selected by user
 TOPIC_WHERE = "SPARQL where clause defined"
