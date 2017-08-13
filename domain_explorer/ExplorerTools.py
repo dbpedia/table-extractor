26c8f998a4955e1847fe7de3c2749fba024ff408
Luca <lucav48@gmail.com>
2017-08-13 12:54:35 +0200

    [ExplorerTools.py] moved method "print_progress_bar" to Utilities class

--- domain_explorer/ExplorerTools.py
+++ domain_explorer/ExplorerTools.py
@@ -269,21 +269,5 @@ class ExplorerTools:
             self.utils.logging.error(msg)
 
     # Print iterations progress
-    def print_progress_bar(self, iteration, total, prefix='Progress: ', suffix='Complete', decimals=1, length=30,
-                           fill='#'):
-        """
-        Call in a loop to create terminal progress bar
-        @params:
-            iteration   - Required  : current iteration (Int)
-            total       - Required  : total iterations (Int)
-            prefix      - Optional  : prefix string (Str)
-            suffix      - Optional  : suffix string (Str)
-            decimals    - Optional  : positive number of decimals in percent complete (Int)
-            length      - Optional  : character length of bar (Int)
-            fill        - Optional  : bar fill character (Str)
-        """
-        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-        filledLength = int(length * iteration // total)
-        bar = fill * filledLength + '-' * (length - filledLength)
-        print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
-        print "\n"
+    def print_progress_bar(self, iteration, total):
+        self.utils.print_progress_bar(iteration, total)
