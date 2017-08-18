993a8b7efdd99b77b8d5aed3853380ef195bbcb3
Luca <lucav48@gmail.com>
2017-08-19 01:41:36 +0200

    [Mapper.py] review method filter_table_data(), insert collaboration with MapperTools class

--- table_extractor/Mapper.py
+++ table_extractor/Mapper.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import rdflib
+from settings import APPLY_FILTER_TO_TABLE_DATA
+
 
 resources_found = []
-
 
 class Mapper:
     """
@@ -43,9 +44,9 @@ class Mapper:
         self.table_data = table_data
         self.utils = utils
         self.logging = utils.logging
-        self.resource = self.utils.delete_accented_characters(resource)
+        self.resource = self.utils.delete_accented_characters(resource).replace("'", "")
         self.dictionary = self.utils.dictionary
-
+        self.mapper = self.utils.mapper
         # Reification_index is and index used to make a reification possible. It use the concept of row.
         self.reification_index = index
 
@@ -58,7 +59,9 @@ class Mapper:
         self.dbp = None
         self.db = None
         self.define_namespace()
-        self.filter_table_data()
+
+        if APPLY_FILTER_TO_TABLE_DATA:
+            self.table_data = self.mapper.filter_table_data(self.table_data, self.table_section)
 
     def define_namespace(self):
         """
@@ -84,18 +87,18 @@ class Mapper:
 
         :return:
         """
-
-        # in row[cell] (that is table's cell value) contains even link in first position
-        for row in self.table_data:
-            # link with between resource section property and each section's row
-            section_property, section_row_property = self.define_row_and_section_property()
-            # if i find a section property that is defined
-            if section_property:
+        # link with between resource section property and each section's row
+        section_property = self.define_row_and_section_property()
+        # if i find a section property that is defined
+        if section_property:
+            # in row[cell] (that is table's cell value) contains even link in first position
+            for row in self.table_data:
+                section_row_property = self.dbr + self.resource + "__" + str(self.reification_index)
                 self.reification_index += 1
                 self.add_triple_to_graph(self.dbr + self.resource, self.dbo + section_property, section_row_property)
                 self.utils.triples_row += 1
                 for cell in row:
-                    value = self.extract_value_from_cell(row[cell])
+                    value = self.mapper.extract_value_from_cell(row[cell])
                     # character "-" means that cell is empty
                     if value != '-':
                         dictionary_property = self.search_on_dictionary(self.table_section + "_" + cell)
@@ -103,32 +106,20 @@ class Mapper:
                             self.add_triple_to_graph(section_row_property, self.dbo + dictionary_property, value,
                                                      type="cell")
                             self.utils.mapped_cells += 1
+        else:
+            # section without mapping rule
+            self.utils.no_mapping_rule_errors_section += 1
+            # all headers will not be mapped, so I have to print it in final report
+            self.utils.no_mapping_rule_errors_headers += len(self.table_data[0])
 
-    def extract_value_from_cell(self, cell):
-        """
-        Cell can be a link and value or only a value
-        :param cell: table's cell
-        :return: value of cell
-        """
-        # take only value's cell and not possible link
-        if len(cell) > 1:
-            value = cell[-1]
-        else:
-            value = cell
-        if isinstance(value, list):
-            return value[0]
-        else:
-            return value
 
     def define_row_and_section_property(self):
         """
         Method to search property of section and to create row's resource with its own index
-        :return: section property and row resource
+        :return: section property resource
         """
-        resource = self.resource.replace("'", "")
         section_property = self.search_on_dictionary(self.table_section)
-        section_row_property = self.dbr + self.resource + "__" + str(self.reification_index)
-        return section_property, section_row_property
+        return section_property
 
     def add_triple_to_graph(self, subject, prop, value, type="row"):
         """
@@ -149,67 +140,11 @@ class Mapper:
         # print subject, prop, value
         self.graph.add((subject, prop, value))
 
-    def check_value_type(self, value):
-        """
-        Check type of a value that I'm analyzing
-        :param value to check
-        :return: value that are casted to a rdflib type (float, string or uri if it's a resource)
-        """
-        # i can have input value like list or like single input, i need to make a filter and get
-        # unique element of this list
-        result = value
-        if self.is_float(result):
-            data_type = rdflib.namespace.XSD.float
-        elif self.is_int(result):
-            data_type = rdflib.namespace.XSD.int
-        else:
-            # If this string represents a resource
-            resource = self.check_if_is_resource(result)
-            if resource:
-                return rdflib.URIRef(resource)
-            else:
-                data_type = rdflib.namespace.XSD.string
-        return rdflib.Literal(result, datatype=data_type)
-
-    def check_if_is_resource(self, resource):
-        """
-        Make a SPARQL query in dbpedia dataset to know if exists a resource named in a particular way
-        :param resource: name of resource that you want to check if it's a reresource
-        :return:
-            - empty if it's not a resource
-            - uri of resource if it exists
-        """
-        resource_to_search = self.adjust_resource(resource)
-        saved_resource = [r for r in resources_found if r in resource_to_search]
-        if not saved_resource:
-            if self.utils.ask_if_resource_exists(self.dbr + self.utils.delete_accented_characters(resource_to_search)):
-                resources_found.append(resource_to_search)
-                result = self.dbr + resource_to_search
-            else:
-                result = ""
-        else:
-            result = self.dbr + saved_resource[-1]
-        return result
-
-    def adjust_resource(self, text):
-        """
-        Remove space from beginning and ending of a string.
-        After that I replace other spaces with _ in order to get a resource suitable for uri
-        :param text: string to process
-        :return: string without blank spaces
-        """
-        if text.startswith(" "):
-            text = text[1:]
-        if text.endswith(" "):
-            text = text[:-1]
-        text = text.replace(" ", "_")
-        return text
-
     def search_on_dictionary(self, key):
         """
         Search over dictionary previously defined for header or section that is passed to this method
         :param key: string that you want to search over dictionary
-        :return: two possible retrive:
+        :return: two possible retrieve:
                 - empty if 'key' is not defined in dictionary
                 - property associated to 'key' if this exists
         """
@@ -238,75 +173,48 @@ class Mapper:
             self.logging.info(message)
             # means a lack of mapping rules
             if value == "":
+                self.utils.no_mapping_rule_errors_headers += 1
                 print message
-                self.utils.no_mapping_rule_errors += 1
         return value
 
-    def is_float(self, value):
+    def check_value_type(self, value):
         """
-        Test out if a value passed as parameter is a float number
-        :param value: a string you want to test
-        :return: True if value is a float | False otherwise
+        Check type of a value that I'm analyzing
+        :param value to check
+        :return: value that are casted to a rdflib type (float, string or uri if it's a resource)
         """
-        try:
-            float(value)
-            return True
-        except TypeError:
-            return False
-        except ValueError:
-            return False
+        # i can have input value like list or like single input, i need to make a filter and get
+        # unique element of this list
+        result = value
+        if self.mapper.is_float(result):
+            data_type = rdflib.namespace.XSD.float
+        elif self.mapper.is_int(result):
+            data_type = rdflib.namespace.XSD.int
+        else:
+            # If this string represents a resource
+            resource = self.check_if_is_resource(result)
+            if resource:
+                return rdflib.URIRef(resource)
+            else:
+                data_type = rdflib.namespace.XSD.string
+        return rdflib.Literal(result, datatype=data_type)
 
-    def is_int(self, value):
+    def check_if_is_resource(self, resource):
         """
-        Test out if a value passed as parameter is an integer
-        :param value: a string you want to test
-        :return: True if the value is an integer | False otherwise
-        """
-        try:
-            int(value)
-            return True
-        except TypeError:
-            return False
-        except ValueError:
-            return False
-
-    def filter_table_data(self):
-        """
-        This method is used to delete last table's row if it represents sum or mean of previous values.
-        It's useful in order to not create triples for this type of information
+        Make a SPARQL query in dbpedia dataset to know if exists a resource named in a particular way
+        :param resource: name of resource that you want to check if it's a reresource
         :return:
+            - empty if it's not a resource
+            - uri of resource if it exists
         """
-        table_list = dict()
-        n = len(self.table_data)
-        i = 0
-        # variables used to count how many cells of last row contains sum or mean of previously cell's value in
-        # same column
-        summarized = 0
-        num_cols = 0
-        for row in self.table_data:
-            for cell in row:
-                value = self.extract_value_from_cell(row[cell])
-                if self.is_float(value) or self.is_int(value):
-                    # i have to avoid last row
-                    if i < n - 1:
-                        try:
-                            new_value = table_list[cell] + value
-                            table_list.__setitem__(cell, new_value)
-                        except KeyError:
-                            table_list.__setitem__(cell, value)
-                    else:
-                        try:
-                            mean_value = (table_list[cell]/(n-1))
-                            # check if last row is sum or mean value of cells
-                            if value == table_list[cell] or str(value) == str(mean_value):
-                                summarized += 1
-                                num_cols = len(row)
-                        except KeyError:
-                            continue
-            i += 1
-        # delete last row if summarized is higher than 1 or 2
-        if summarized >= 2:
-            del self.table_data[-1]
-            # for a well done statistic, i have to decrease data_extracted so that it will not represents
-            # even summary rows (like Career in athlete tables)
-            self.utils.data_extracted -= num_cols
+        resource_to_search = self.mapper.adjust_resource(resource)
+        saved_resource = [r for r in resources_found if r in resource_to_search]
+        if not saved_resource:
+            if self.utils.ask_if_resource_exists(self.dbr + self.utils.delete_accented_characters(resource_to_search)):
+                resources_found.append(resource_to_search)
+                result = self.dbr + resource_to_search
+            else:
+                result = ""
+        else:
+            result = self.dbr + saved_resource[-1]
+        return result
